964b79813013e2112a6ec6bf8bbc565255e218b0
Minskoleopc <minskoleopc@gmail.com>
2023-06-20 07:48:23 +0530

    added

--- src/one/day12.java
+++ src/one/day12.java
@@ -45,7 +45,7 @@ public class day12 {
 
 		System.out.println(rev2);
 
-//   
+//   METHODS --------> 
 
 		// Program 1
 		String city1 = "Pune";
@@ -92,6 +92,137 @@ public class day12 {
 
 		String s2 = city6.substring(1, 3);
 		System.out.println(s2);
+		
+		
+		// StartsWith() 
+		String city8 = "chandrapur";
+		boolean s3 = city8.startsWith("c");
+		System.out.println(s3);
+		
+		// EndsWith()
+		String city9 = "bhopal";
+		boolean s4 = city9.endsWith("pal");
+		System.out.println(s4);
+		
+		// Split()
+		// come back to this after array
+		String city10 = "I am learning javascript";
+		String [] s5 = city10.split(" "); //{"I","am","learning","javascript"}
+		String [] s6 = city10.split("a"); //{"I ,"m" "le","rning j","v","script"}
+		
+		//replaceFirst()
+		String city11 = "I am learning python and python is good language";
+		String s7 = city11.replaceFirst("python", "java");
+		System.out.println(s7);
+		
+		//replaceAll()
+		String s8 = city11.replaceAll("python", "java");
+		System.out.println(s8);
+		
+		//replace()
+		String s9 = city11.replace("I", "W");
+		System.out.println(s9);
+		
+		//concat()
+		String city13 = "pune";
+		String city14 = "Welcome to ";
+		String s10 = city14.concat(city13);
+		System.out.println(s10);
+		
+		// contains()
+		
+		String city15 = "I am learning js in bhopal";
+		boolean s11 = city15.contains("learning");
+		System.out.println(s11);
+		
+		
+		// equal()
+
+		String city16 = "pune";
+		String city17 = "PUNE";
+		
+		boolean s12 = city16.equals(city17);
+		System.out.println(s12);
+		
+		
+		//equalIgnoreCase()
+		boolean s13 = city16.equalsIgnoreCase(city17);
+		System.out.println(s13);
+		
+		
+		
+		// trim()
+		String city18 = " I am learning js ";
+		System.out.println(city18.length());
+		String s14 = city18.trim();
+		System.out.println(s14.length());
+		
+		
+		// contains 
+		
+		String city19 = "I am learning spanish";
+		String city20 = "SPANISH";
+		boolean s15 = city19.toUpperCase().contains(city20);
+		System.out.println(s15);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+ 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
